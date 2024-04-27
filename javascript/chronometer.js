2f2f810d7b56fa20a5cf96daccddd69f430f45c4
Tais Taschetto <taistaschetto@hotmail.com>
2024-04-27 15:37:50 +0200

    Iteracion 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (callback) {
+        callback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return `${value}`.padStart(2, 0);
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
+    document.getElementById('minDec').innerHTML = '00';
+    document.getElementById('minUni').innerHTML = '00';
+
+    document.getElementById('secDec').innerHTML = '00';
+    document.getElementById('secUni').innerHTML = '00';
+
+    document.getElementById('milDec').innerHTML = '00';
+    document.getElementById('milUni').innerHTML = '00';
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
   }
 }
 
